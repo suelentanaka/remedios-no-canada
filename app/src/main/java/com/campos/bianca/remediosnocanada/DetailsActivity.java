a9eeccce28d9a775333ed0a23534c4cf28528a27
Bianca Campos <biancancampos@gmail.com>
2018-07-19 09:47:20 -0700

    First layout and strings pt-BR

---
+++ app/src/main/java/com/campos/bianca/remediosnocanada/DetailsActivity.java
@@ -0,0 +1,4 @@
+package com.campos.bianca.remediosnocanada;
+
+public class DetailsActivity {
+}
--- app/src/main/java/com/campos/bianca/remediosnocanada/MainActivity.java
+++ app/src/main/java/com/campos/bianca/remediosnocanada/MainActivity.java
@@ -2,6 +2,7 @@ package com.campos.bianca.remediosnocanada;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listview = (ListView) findViewById(R.id.medicine_LV);
     }
 }
